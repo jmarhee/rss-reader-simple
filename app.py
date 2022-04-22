69f695684c782073e6fd61027af0bae7d0e244dc
Joseph D. Marhee <jmarhee@interiorae.com>
2022-04-22 18:52:22 +0000

    Updates feed list reader to be scheduled task rather than on every page load so only the first load after startup requires a full read, subsequent checks in background
    Signed-off-by: Joseph D. Marhee <jmarhee@interiorae.com>

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
 from parser import buildFeed, buildConfig
 from flask import Flask, render_template
 import os
+from apscheduler.schedulers.background import BackgroundScheduler
  
 app = Flask(__name__)
 
+feed_data = None
+
+@app.before_first_request
+def initialize_feeds():
+	feeds = buildConfig()
+	global feed_data
+	feed_data = buildFeed(feeds)
+	return feed_data
+
+def update_feeds():
+	feeds = buildConfig()
+	global feed_data
+	feed_data = buildFeed(feeds)
+	return feed_data
+
+scheduler = BackgroundScheduler()
+job = scheduler.add_job(update_feeds, 'interval', minutes=1)
+scheduler.start()
+
 @app.route('/')
 def index():
-	feeds = buildConfig()
-	feeds_data = buildFeed(feeds)
 	if os.environ.get("SITE_NAME") == "":
 		site_name = "reader.freeipad.internal"
 	else:
 		site_name = os.environ['SITE_NAME']
-	return render_template('index.html', feed_data=feeds_data, site_name=site_name)
+	return render_template('index.html', feed_data=feed_data, site_name=site_name)
+
+scheduler = BackgroundScheduler()
+job = scheduler.add_job(update_feeds, 'interval', minutes=1)
+scheduler.start()
